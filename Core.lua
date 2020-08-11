5829fb873942bcfd32f2e0303f8b9d8e401c3991
Lindsey <lindsey@phparadox.com>
2020-08-10 21:07:41 -0400

    debug flags?

--- Core.lua
+++ Core.lua
@@ -95,21 +95,22 @@ local VERSION_ValidRaid = 4
 -----------------------
 -- Utility Functions --
 -----------------------
-
-local function dump(o)
+--@debug@
+function DBG_dump(o)
 	if type(o) == 'table' then
 	   local s = '{ '
 	   for k,v in pairs(o) do
 		  if type(k) ~= 'number' then k = '"'..k..'"' end
-		  s = s .. '['..k..'] = ' .. dump(v) .. ','
+		  s = s .. '['..k..'] = ' .. DBG_dump(v) .. ','
 	   end
 	   return s .. '} '
 	else
 	   return tostring(o)
 	end
 end
+--@end-debug@
 
- local function explode(seperator, content)
+local function explode(seperator, content)
 	local t, position
 	t = {}
 	position = 0
